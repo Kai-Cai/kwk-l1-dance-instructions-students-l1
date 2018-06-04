80b83579db537af24f21833e9be1eba8b5011b50
Kai-Cai <kaiaschiff@gmail.com>
2018-06-04 22:42:33 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,10 @@
+local_greeting = "Hey"
+your_name = "Randy"
+my_name = "Kaia"
+time_of_day = "Summertime"
+
+def greeting
+  puts "#{local_greeting} next #{your_name}, I'm #{my_name}."
+end  
+
+puts greeting
